be2162cebce002aff11b583cf06b606442192280
AtharvaN29 <narvekaratharva29@gmail.com>
2024-08-20 00:56:29 +0530

    plot perf added

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -7,10 +7,10 @@ import { Hero } from "@/components/Hero";
 const user = {
   rank: 'pupil',
   imageUrl: 'https://www.istockphoto.com/photo/colorful-panther-chameleon-gm1052202540-281251557?utm_campaign=category_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fimages%2Fanimals&utm_medium=affiliate&utm_source=unsplash&utm_term=Animals+Images+%26+Pictures%3A%3A%3A',
-  name: 'atharva_n29',
-  email: 'narvekaratharva29@gmail.com',
-  joinedDate: '2024-01-01',
-  college: 'DJSCE',
+  name: 'dummy',
+  email: 'dummy',
+  joinedDate: 'dummy',
+  college: 'dummy',
   city: 'Mumbai',
   country: 'India',
   contestRating: 1000,
@@ -37,7 +37,7 @@ export default function Home() {
     <main className="flex min-h-screen min-w-screen flex-col items-center justify-between">
       <Navbar></Navbar>
       <Hero></Hero>
-
+      <Analyzer user={user} stats={stats}/> 
     </main>
   );
 }
