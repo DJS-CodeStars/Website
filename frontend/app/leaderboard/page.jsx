874a542b2e96aa99bc5c6e0697356639a122c583
AmodJ10 <amodjoshi011@gmail.com>
2024-08-20 23:08:51 +0530

    Added Leaderboard

--- frontend/app/leaderboard/page.jsx
+++ frontend/app/leaderboard/page.jsx
@@ -1,14 +1,16 @@
 'use client'
 import { Navbar } from "@/components/Navbar";
-import ProblemRecommender from "@/components/ProblemRecommender";
-import Leaderboard  from "@/components/Leaderboard";
+// import ProblemRecommender from "@/components/ProblemRecommender";
+import Leaderboard from "@/components/Leaderboard";
+
 import '@/app/globals.css'
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center">
       <Navbar />
-      <ProblemRecommender />
-      <Leaderboard />
+      {/* <ProblemRecommender /
+      > */}
+    <Leaderboard/>
     </div>
   );
 }
