18c5626f5add5508ccd80b7c8cb432b7511f879f
AtharvaN29 <narvekaratharva29@gmail.com>
2024-08-21 17:24:24 +0530

    analyzer updated

--- frontend/components/Analyzer.js
+++ frontend/components/Analyzer.js
@@ -2,45 +2,8 @@
 import React, { useState, useEffect } from "react";
 import Plot from "react-plotly.js";
 
-const UserInfoCard = ({ user }) => {
-  return (
-    <div className="bg-green-200 p-4 rounded-lg shadow-md mb-6 flex-col justify-center items-center mt-5  ">
-      {/* <img
-        src={userImage}
-        alt={`${user.name}'s profile`}
-        className="w-16 h-16 rounded-full mr-4"
-      /> */}
-      <h4 className="text-green-700 text-md font-semibold">{user.rank}</h4>
-      <h2 className="text-green-700 text-2xl font-semibold mb-2">
-        {user.name}
-      </h2>
-      <p className="text-black">Email: {user.email}</p>
-      <p className="text-black">Joined: {user.joinedDate}</p>
-      <p className="text-black">College: {user.college}</p>
-      <p className="text-black">Country: {user.country}</p>
-      <p className="text-black">City: {user.city}</p>
-      <p className="text-black">Contest Rating: {user.contestRating}</p>
-    </div>
-  );
-};
-
-const UserStats = ({ stats }) => {
-  return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
-      {Object.keys(stats).map((key, index) => (
-        <div
-          key={index}
-          className="bg-green-200 p-2 rounded-lg shadow-md text-center"
-        >
-          <h3 className="text-lg font-medium text-black">{key}</h3>
-          <p className="text-xl font-bold text-black">{stats[key]}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
-
 const Analyzer = ({ user, stats }) => {
+  const [data, setData] = useState(null);
   const [handle, setHandle] = useState("");
   const [graphData, setGraphData] = useState(null);
   const [RatinggraphData, setRatingGraphData] = useState(null);
@@ -53,6 +16,7 @@ const Analyzer = ({ user, stats }) => {
       .then((response) => response.json())
       .then((data) => {
         if(data.date && data.delta){
+          setData(data);
           setGraphData({
             x: data.date,
             y: data.delta,
@@ -67,11 +31,6 @@ const Analyzer = ({ user, stats }) => {
       .catch((error) => console.error("Error fetching data:", error));
   };
 
-  useEffect(() => {
-    if (handle) {
-      fetchData();
-    }
-  }, [handle]);
 
   return (
     <div className="p-6 flex-col justify-center items-center w-screen">
@@ -86,19 +45,70 @@ const Analyzer = ({ user, stats }) => {
         />
         <button
           onClick={fetchData}
-          className="ml-4 p-3 bg-blue-500 text-white rounded-md"
+          className="ml-4 p-3 bg-yellow-500 text-black rounded-md"
         >
           Get Stats
         </button>
       </div>
-      <div className="flex flex-row justify-center items-center">
-        <div className="m-5">
-          <UserInfoCard user={user} />
-        </div>
-        <div className="m-5">
-          <UserStats stats={stats} />
-        </div>
-      </div>
+      {/* {data && ( */}
+        <div className="flex flex-row justify-center items-center">
+          <div className="flex basis-1/4 m-5 justify-center items-center flex-col border-2 border-yellow-500 p-5 rounded-md">
+              <p className="font-mono text-lg">Title:Content</p>
+              <p className="font-mono text-lg">Title:Content</p>
+              <p className="font-mono text-lg">Title:Content</p>
+              <p className="font-mono text-lg">Title:Content</p>
+              <p className="font-mono text-lg">Title:Content</p>
+              <p className="font-mono text-lg">Title:Content</p>
+              <p className="font-mono text-lg">Title:Content</p>
+          </div>
+          <div className="flex basis-1/4 m-5 flex-col justify-center items-center">
+              <div className="flex justify-center items-center">
+                <div className="flex justify-center items-center m-3 p-3 border-2 border-yellow-500 rounded-md flex-col">
+                  <div><p className="font-mono text-lg">title</p></div>
+                  <div><p className="font-mono text-2xl">content</p></div>
+                </div>
+                <div className="flex justify-center items-center m-3 p-3 border-2 border-yellow-500 rounded-md flex-col">
+                  <div><p className="font-mono text-lg">title</p></div>
+                  <div><p className="font-mono text-2xl">content</p></div>
+                </div>
+                <div className="flex justify-center items-center m-3 p-3 border-2 border-yellow-500 rounded-md flex-col">
+                  <div><p className="font-mono text-lg">title</p></div>
+                  <div><p className="font-mono text-2xl">content</p></div>
+                </div>
+                <div className="flex justify-center items-center m-3 p-3 border-2 border-yellow-500 rounded-md flex-col">
+                  <div><p className="font-mono text-lg">title</p></div>
+                  <div><p className="font-mono text-2xl">content</p></div>
+                </div>
+                <div className="flex justify-center items-center m-3 p-3 border-2 border-yellow-500 rounded-md flex-col">
+                  <div><p className="font-mono text-lg">title</p></div>
+                  <div><p className="font-mono text-2xl">content</p></div>
+                </div>
+              </div>
+              <div className="flex justify-center items-center">
+                <div className="flex justify-center items-center m-3 p-3 border-2 border-yellow-500 rounded-md flex-col">
+                  <div><p className="font-mono text-lg">title</p></div>
+                  <div><p className="font-mono text-2xl">content</p></div>
+                </div>
+                <div className="flex justify-center items-center m-3 p-3 border-2 border-yellow-500 rounded-md flex-col">
+                  <div><p className="font-mono text-lg">title</p></div>
+                  <div><p className="font-mono text-2xl">content</p></div>
+                </div>
+                <div className="flex justify-center items-center m-3 p-3 border-2 border-yellow-500 rounded-md flex-col">
+                  <div><p className="font-mono text-lg">title</p></div>
+                  <div><p className="font-mono text-2xl">content</p></div>
+                </div>
+                <div className="flex justify-center items-center m-3 p-3 border-2 border-yellow-500 rounded-md flex-col">
+                  <div><p className="font-mono text-lg">title</p></div>
+                  <div><p className="font-mono text-2xl">content</p></div>
+                </div>
+                <div className="flex justify-center items-center m-3 p-3 border-2 border-yellow-500 rounded-md flex-col">
+                  <div><p className="font-mono text-lg">title</p></div>
+                  <div><p className="font-mono text-2xl">content</p></div>
+                </div>
+              </div>
+          </div>
+        </div> 
+      {/* )} */}
       <div className="flex flex-row">
         {/* Performance plot */}
 
